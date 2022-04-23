d6967c3c93b03fdbf0d8d62d162260c0ee00227f
Dominik Trippel <domnik.trippel@gmail.com>
2022-04-24 00:03:54 +0200

    added first program

---
+++ mining/stripmine.lua
@@ -0,0 +1,20 @@
+local gps = require("gps")
+local fs = require("fs")
+
+local function savePosition()
+    local x, y, z = gps.locate()
+    file = fs.open("./pos.txt", "w")
+    file.write(string.format("%s\n%s\n%s", x, y, z))
+    file.close()
+end
+
+local function readPosition()
+    file = fs.open("./pos.txt", "w")
+    x = file.readLine()
+    y = file.readLine()
+    z = file.readLine()
+    return x, y, z
+end
+
+savePosition()
+print(readPosition())
